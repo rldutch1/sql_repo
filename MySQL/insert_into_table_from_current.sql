a7dc8fead4f14373f21048d6c9246b5240170aa6
Robert Holland <rob@robholland.com>
2014-07-26 21:53:15 -0700

    Insert data into a second table from a first table.

---
+++ MySQL/insert_into_table_from_current.sql
@@ -0,0 +1,10 @@
+/*
+INSERT INTO ANOTHER TABLE FROM CURRENT TABLE
+You can make an empty version (copy) of a table by using CREATE TABLE.
+Example:
+*/
+CREATE TABLE secondtable LIKE firsttable;
+
+-- This example inserts two columns into the secondtable table from the firsttable table.
+
+insert into secondtable (column1, column2) select column1, column2 from firsttable;
