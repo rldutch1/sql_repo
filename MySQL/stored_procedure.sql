85e14d07337fc848fc1f5d00dd997ea77916a256
Robert Holland <rob@robholland.com>
2015-04-17 09:42:28 -0700

    stored_procedure.sql: Added some more notes to the file. I am using stored procedures in my iviewphase4 database.

--- MySQL/stored_procedure.sql
+++ MySQL/stored_procedure.sql
@@ -1,8 +1,11 @@
 -- http://dev.mysql.com/doc/connector-net/en/connector-net-tutorials-stored-procedures.html
+-- To view all procedures.
+	show procedure status;
 
+-- Stored procedure with parameter.
 DELIMITER //
 CREATE PROCEDURE sersearch
-(IN sese CHAR(200))
+(IN sese CHAR(200)) /* The sese is the parameter. Make it the same data type as the field it represents.*/
 BEGIN
   SELECT software, license, ownername FROM serials
   WHERE software like sese;
@@ -14,3 +17,18 @@ DELIMITER ;
 
 -- Example, type:
 	call sersearch ('Adobe%'); -- Can use the wildcard in the datafield when searching.
+
+-- Stored procedure with no parameter.
+DELIMITER //
+CREATE PROCEDURE sersearch
+() /* The parenthesis must be present but empty for no parameter.*/
+BEGIN
+  SELECT software, license, ownername FROM serials;
+END //
+DELIMITER ;
+
+-- To execute the stored procedure use the following syntax:
+	call procedurename;
+
+-- Example, type:
+	call sersearch; -- No parameter.
