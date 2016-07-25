0b1b880f4109551082339903494d2ea7a4b7cc23
Robert Holland <rob@robholland.com>
2016-07-25 15:57:15 -0700

    Added more notes to the foreign_key_constraint.sql file.

--- MySQL/foreign_key_constraint.sql
+++ MySQL/foreign_key_constraint.sql
@@ -16,6 +16,8 @@ CREATE TABLE `mfg` (
 I added this FK constraint later:
 alter table hserials add constraint mfg_id2hser_mfg_id foreign key (mfg_id) references mfg(id) on update cascade on delete restrict;
 
+NOTE: The column data types have to be the same before you can add a foreign key constraint. I had a problem creating a constraint on an int(11) unsigned because one of the columns did not have the "Unsigned" attribute.
+
 hserials Table (from show create table mfg): 
 CREATE TABLE `hserials` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -33,8 +35,28 @@ CREATE TABLE `hserials` (
 To find out the symbol value when you want to drop a foreign key, use the SHOW CREATE TABLE statement. For example:
 show create table hserials\G;
 
+ALTER TABLE hserials DROP FOREIGN KEY `hw_serial1`;
+
 To display a detailed explanation of the most recent InnoDB foreign key error in the server, issue SHOW ENGINE INNODB STATUS.
 
 select h.mfg_id, m.id from hserials h left join mfg m on (h.mfg_id=m.id) where h.mfg_id = m.id;
 
+To add an index to a column:
+	alter table tablename add index (columnname);
 
+To add multi-column unique constraint: 
+	create table testnames (
+	id int(11) unsigned auto_increment primary key, 
+	firstname varchar(100) not null, 
+	lastname varchar(100) not null
+	) engine=innodb;
+Then type:
+	alter table testnames add unique fname_lname_index(firstname, lastname);
+
+Or you can add the unique constraint when you create the table:
+	create table testnames (
+	id int(11) unsigned auto_increment primary key, 
+	firstname varchar(100) not null, 
+	lastname varchar(100) not null
+	unique key fname_lname_index(firstname, lastname)
+	) engine=innodb;
