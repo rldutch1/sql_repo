7f3dc40d625ef01b58c5ba1207681e7839cca7f2
Robert Holland <rob@robholland.com>
2016-07-26 00:59:23 -0700

    Added more information to triggers.sql.

---
+++ MySQL/triggers.sql
@@ -0,0 +1,25 @@
+https://www.youtube.com/watch?v=BeG4IHFjqVg
+Triggers are tied or attached to tables.
+
+show triggers;
+drop trigger triggername;
+
+create trigger triggername triggertime triggerevent on tablename
+for each row triggerstatement;
+1. triggername - Name of trigger.
+2. triggertime - Before or After.
+3. triggerevent - Insert, Update, or Delete event.
+4. triggerstatement - A standard SQL statement. Generally is a single statement but can be multiple statements. 
+
+The default MySQL statement delimeter is a semi-colon.
+
+You have to use a different delimeter when creating a trigger.
+
+delimeter !
+	create trigger triggername triggertime triggerevent on tablename
+	for each row triggerstatement;
+	statement1;
+	statement2;
+	statementn;
+	end;
+	delimeter ;
