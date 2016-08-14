38004bcd6f146aaf5eb5f9aa5be40d63da99e23c
Robert Holland <rob@robholland.com>
2016-08-14 10:26:14 -0700

    Cleaned up the foreign key constraint query so that it will show the database name, table name, and column name that references another database, table, and column name.

--- MySQL/foreign_key_constraint.sql
+++ MySQL/foreign_key_constraint.sql
@@ -8,7 +8,7 @@ show create table xxxxx\G
 
 All tables foreign key query:
 	use INFORMATION_SCHEMA;
-	select concat(TABLE_NAME,'.',COLUMN_NAME) as This_Table_Column,CONSTRAINT_NAME, concat(REFERENCED_TABLE_NAME,'.',REFERENCED_COLUMN_NAME) as References_This_Table_Column from KEY_COLUMN_USAGE where TABLE_SCHEMA = "videos" and referenced_table_name is not NULL;
+	select concat(TABLE_SCHEMA,'.',TABLE_NAME,'.',COLUMN_NAME) as This_DB_Table_and_Column, concat(TABLE_SCHEMA,'.',REFERENCED_TABLE_NAME,'.',REFERENCED_COLUMN_NAME) as References_DB_Table_and_Column,CONSTRAINT_NAME from KEY_COLUMN_USAGE where referenced_table_name is not NULL;
 
 Specific database and table foreign key query:
 	use INFORMATION_SCHEMA;
