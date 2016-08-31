135c745ca5e1643aab94db6208556ce091a5ad77
Robert Holland <rob@robholland.com>
2016-08-31 01:31:42 -0700

    Biometrics and Vetting databases.

---
+++ MySQL/SQL/passagepoint.shipped_software.sql
@@ -0,0 +1,45 @@
+/*
+Author: Robert Holland
+Purpose: Create Passagepoint Shipped_Software table that shows who the software was shipped to.
+Date: 7 April 2013 20:01 +3
+*/
+    
+create table pp_shipped_software ( id int(11) unsigned auto_increment primary key 
+, shipped_to int(11) not null
+, soft_id int(11) not null
+, mfg_id int(11) not null
+, name varchar(100) not null
+, license varchar(100) not null
+, owner varchar(100) not null);
+
+/*Contact ID will be 8 Luis and 9 Nick */
+
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values (
+
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('9','1','2','PP GLOBAL CLIENT LICENSE','9TAHJ7SE','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('9','2','2','PP GLOBAL CLIENT LICENSE','7BDDKASQ','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('9','3','2','PP GLOBAL CLIENT LICENSE','W6QFSEBZ','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('9','4','2','PP GLOBAL CLIENT LICENSE','5FZYQSE3','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('8','5','2','PP GLOBAL CLIENT LICENSE','26EV4KFZ','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('8','6','2','PP GLOBAL CLIENT LICENSE','MJ2RTDP2','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('8','7','2','PP GLOBAL CLIENT LICENSE','CWCRYBPK','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('8','8','2','PP GLOBAL CLIENT LICENSE','74GM69DK','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','9','2','PP GLOBAL CLIENT LICENSE','TNWG9FH5','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','10','2','PP GLOBAL CLIENT LICENSE','VRCW22F6','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','11','2','PP GLOBAL CLIENT LICENSE','SFB6F264','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','12','2','PP GLOBAL CLIENT LICENSE','P2TVQXR6','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','13','2','PP GLOBAL CLIENT LICENSE','3JWJ3KBP','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','14','2','PP GLOBAL CLIENT LICENSE','6CZPUMUZ','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','15','2','PP GLOBAL CLIENT LICENSE','VVFCBUEY','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','16','2','PP GLOBAL CLIENT LICENSE','DTD8Y8TA','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','17','2','PP GLOBAL CLIENT LICENSE','DXQP24HK','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','18','2','PP GLOBAL CLIENT LICENSE','VA6BNHW3','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','19','2','PP GLOBAL CLIENT LICENSE','C7NKPKCY','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','20','2','PP GLOBAL CLIENT LICENSE','WXCT35VF','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','21','2','PP GLOBAL CLIENT LICENSE','JBWNY4T7','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','22','2','PP GLOBAL CLIENT LICENSE','E57RBQWH','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','23','2','PP GLOBAL CLIENT LICENSE','T85J4SE4','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','24','2','PP GLOBAL CLIENT LICENSE','SPBU7FP7','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('0','25','2','PP GLOBAL CLIENT LICENSE','6EQE2SE4','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('9','26','2','Directory Link','JTB5NHZ4','U.S. Department of State');
+insert into pp_shipped_software (shipped_to, soft_id, mfg_id, name, license, owner) values ('9','27','2','Control Center','GAHHAK8Z','U.S. Department of State');
