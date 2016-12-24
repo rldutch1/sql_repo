c64274da5f51ef24e2d771667c8855367abd9783
Robert Holland <Robert.Holland@bannerhealth.com>
2016-12-23 17:12:16 -0700

    Added commands for renaming a table and renaming a column. Added relational database commands for referencing another column in another table as well as the current table. Also added drop constraint command.

--- MSSQL/altertable.sql
+++ MSSQL/altertable.sql
@@ -13,6 +13,27 @@
 	alter table tablename add columnname varchar(20) null
     constraint constraintname unique;
 
+--* Dropping a contraint:
+	alter table tablename drop constraint constraintname;
+
+--Add multiple columns in table
+--You can use the ALTER TABLE statement in SQL Server to add multiple columns to a table.
+--Syntax
+--The syntax to add multiple columns to an existing table in SQL Server (Transact-SQL) is:
+
+	ALTER TABLE table_name
+  	ADD column_1 column-definition,
+    column_2 column-definition,
+    ...
+    column_n column_definition;
+
+--Example
+--Let's look at an example that shows how to add multiple columns to a table in SQL Server using the ALTER TABLE statement.
+--For example:
+
+	ALTER TABLE employees ADD last_name VARCHAR(50), first_name VARCHAR(40);
+--This SQL Server ALTER TABLE example will add two columns, last_name as a VARCHAR(50) field and first_name as a VARCHAR(40) field to the employees table.
+
 --* Adding an unverified CHECK constraint to an existing column
 	The following example adds a constraint to an existing column in the table.
 	The column has a value that violates the constraint.
@@ -35,7 +56,6 @@
 	CREATE TABLE tablename ( columnname INT CONSTRAINT constraintname UNIQUE) ;
 	GO
 
-
 --* Add a PRIMARY KEY identity column.
 	ALTER TABLE tablename ADD
 	newcolumn INT IDENTITY
@@ -46,6 +66,10 @@
 	newcolumn INT NULL
 	CONSTRAINT newcolumn_fk
 	REFERENCES tablename(columnname),
+
+--* Add a column that references another column in another table.
+alter table this_tablename add constraint FK_Name foreign key (this_tablename_column)
+references another_table (another_table_column) on delete cascade on update cascade
 
 --* Add a column with a constraint to enforce that
 --* nonnull data is in a valid telephone number format.
@@ -68,6 +92,36 @@
 	DROP TABLE tablename ;
 	GO
 
---* Rename a column.
-	exec sp_rename 'thetablename.theoldcolumnname', 'thenewcolumnname', 'column';
+--Rename column in table
+--You can not use the ALTER TABLE statement in SQL Server to rename a column in a table. However, you can use sp_rename, though Microsoft recommends that you drop and recreate the table so that scripts and stored procedures are not broken.
+--Syntax
+--The syntax to rename a column in an existing table in SQL Server (Transact-SQL) is:
+
+	exec sp_rename 'table_name.old_column_name', 'new_column_name', 'COLUMN';
 	--You will probably see a warning message that renaming a table can break scripts and stored procedures.
+
+--Example
+--Let's look at an example that shows how to rename a column in a table in SQL Server using sp_rename.
+--For example:
+
+	sp_rename 'employees.last_name', 'lname', 'COLUMN';
+--This SQL Server example will use sp_rename to rename the column in the employees table from last_name to lname.
+
+--Rename table
+--You can not use the ALTER TABLE statement in SQL Server to rename a table. However, you can use sp_rename, though Microsoft recommends that you drop and recreate the table so that scripts and stored procedures are not broken.
+--Syntax
+--The syntax to rename a table in SQL Server (Transact-SQL) is:
+
+	sp_rename 'old_table_name', 'new_table_name';
+
+--Example
+--Let's look at an example that shows how to rename a table in SQL Server using sp_rename.
+--For example:
+
+	sp_rename 'employees', 'emps';
+--This SQL Server example will use sp_rename to rename the employees table to emps.
+
+--Source: https://www.techonthenet.com/sql_server/tables/alter_table.php
+
+--View "Describe" type information about the table.
+	exec sp_columns tablename;
