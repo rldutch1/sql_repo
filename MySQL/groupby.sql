dd7ffb5b38cf587946322c59bec27bbadf0b0991
Robert Holland <rob@robholland.com>
2017-02-13 23:33:52 -0700

    Added another group by example.

--- MySQL/groupby.sql
+++ MySQL/groupby.sql
@@ -2,3 +2,5 @@
 
 SELECT column1, COUNT(*) c FROM tablename GROUP BY column1 HAVING c > 1;
 
+-- Show number of presidents from each state.
+select state_born State, count(*) Number_of_Presidents from uspresidents group by state having Number_of_Presidents > 0 order by Number_of_Presidents, state;
