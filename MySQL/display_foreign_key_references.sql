c12f7ea6c3bb8e92129e5c1c694cc46cafcb65eb
Robert Holland <rob@robholland.com>
2017-06-10 22:50:20 -0700

    Adding query that will display the foreign key referencs. It can be run from the command line. Look inside file for instructions.

---
+++ MySQL/display_foreign_key_references.sql
@@ -0,0 +1,5 @@
+-- To run from the command prompt type:
+-- mysql -u rob -p < display_foreign_key_references.sql > current_fk_refs.txt
+
+use INFORMATION_SCHEMA;
+select concat(TABLE_SCHEMA,'.',TABLE_NAME,'.',COLUMN_NAME) as This_DB_Table_and_Column, concat(TABLE_SCHEMA,'.',REFERENCED_TABLE_NAME,'.',REFERENCED_COLUMN_NAME) as References_DB_Table_and_Column,CONSTRAINT_NAME from KEY_COLUMN_USAGE where referenced_table_name is not NULL order by This_DB_Table_and_Column;
