9a41c34da711a47f1734760bf2949e51acf563ac
Robert Holland <rob@robholland.com>
2017-06-10 22:52:22 -0700

    Added comment to display_foreign_key_references.sql.

--- MySQL/display_foreign_key_references.sql
+++ MySQL/display_foreign_key_references.sql
@@ -3,3 +3,5 @@
 
 use INFORMATION_SCHEMA;
 select concat(TABLE_SCHEMA,'.',TABLE_NAME,'.',COLUMN_NAME) as This_DB_Table_and_Column, concat(TABLE_SCHEMA,'.',REFERENCED_TABLE_NAME,'.',REFERENCED_COLUMN_NAME) as References_DB_Table_and_Column,CONSTRAINT_NAME from KEY_COLUMN_USAGE where referenced_table_name is not NULL order by This_DB_Table_and_Column;
+
+-- The order by clause may not work on all DB's. If there is an error remove it and run again.
