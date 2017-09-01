ad5fbb77d8d3d74e2a97fd3802bb0839e213439f
Robert Holland <rob@robholland.com>
2017-09-01 16:40:29 -0700

    Minor change.

--- MySQL/display_foreign_key_references.sql
+++ MySQL/display_foreign_key_references.sql
@@ -2,7 +2,7 @@
 -- mysql -u rob -p < display_foreign_key_references.sql > current_fk_refs.txt
 
 use INFORMATION_SCHEMA;
-select concat(TABLE_SCHEMA,'.',TABLE_NAME,'.',COLUMN_NAME) as This_DB_Table_and_Column, concat(TABLE_SCHEMA,'.',REFERENCED_TABLE_NAME,'.',REFERENCED_COLUMN_NAME) as References_DB_Table_and_Column,CONSTRAINT_NAME from KEY_COLUMN_USAGE where referenced_table_name is not NULL order by This_DB_Table_and_Column;
+select concat(TABLE_SCHEMA,'.',TABLE_NAME,'.',COLUMN_NAME) as This_DB_Table_and_Column, concat(TABLE_SCHEMA,'.',REFERENCED_TABLE_NAME,'.',REFERENCED_COLUMN_NAME) as References_This_DB_Table_and_Column,CONSTRAINT_NAME from KEY_COLUMN_USAGE where referenced_table_name is not NULL order by This_DB_Table_and_Column;
 
 -- The order by clause may not work on all DB's. If there is an error remove it and run again.
 
@@ -10,7 +10,7 @@ select concat(TABLE_SCHEMA,'.',TABLE_NAME,'.',COLUMN_NAME) as This_DB_Table_and_
 use INFORMATION_SCHEMA;
 select
 concat(kcu.TABLE_SCHEMA,'.',kcu.TABLE_NAME,'.',kcu.COLUMN_NAME) as This_DB_Table_and_Column,
-concat(kcu.TABLE_SCHEMA,'.',kcu.REFERENCED_TABLE_NAME,'.',kcu.REFERENCED_COLUMN_NAME) as References_DB_Table_and_Column,
+concat(kcu.TABLE_SCHEMA,'.',kcu.REFERENCED_TABLE_NAME,'.',kcu.REFERENCED_COLUMN_NAME) as References_This_DB_Table_and_Column,
 kcu.CONSTRAINT_NAME,
 rc.update_rule,
 rc.delete_rule
