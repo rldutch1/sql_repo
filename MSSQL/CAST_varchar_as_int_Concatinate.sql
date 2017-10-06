13ca0c5de877f2a271b10a3441b94fb75d0e9bd3
Robert Holland <Robert.Holland@bannerhealth.com>
2017-10-06 09:28:02 -0700

    Added another example for concatinating data fields in MSSQL. MSSQL 2012 has the concat() function.

--- MSSQL/CAST_varchar_as_int_Concatinate.sql
+++ MSSQL/CAST_varchar_as_int_Concatinate.sql
@@ -1,13 +1,17 @@
-/* Shows how to select a varchar as an integer and vice-versa.
+/* Shows how to select a varchar as an integer and vice-versa.*/
 
-Code Snippet
+-- Code Snippet
 SELECT (CAST Col1 AS int) AS Col1
 
 and if you need it to be back into character format....
 
-Code Snippet
+-- Code Snippet
 SELECT (CAST(CAST Col1 AS int) AS varchar(length)) AS Col1
 
-In this example, the ID column is an integer and I needed it to be a string before I concatinated the two columns.
-There is no "Concat" feature in MSSQL before version 2012. Below is how to concatinate two columns.
+-- In this example, the ID column is an integer and I needed it to be a string before I concatinated the two columns.
+-- There is no "Concat" feature in MSSQL before version 2012. Below is how to concatinate two columns.
 select (cast(id as varchar(200)) + ' - ' + dta) as "Concatinated" from dta_search;
+
+
+-- This method uses the plus sign without casting. It is the old school way that I did it many moons ago:
+select [columnname], 'blah', 'blah', 'blah', [columnname] from vw_tablename order by columnname;
