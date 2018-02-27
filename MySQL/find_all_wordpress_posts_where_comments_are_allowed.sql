bc937d37b27d0a5ea0e65f00a20f5bd92f6c08e3
Robert Holland <rob@robholland.com>
2018-02-27 09:05:44 -0700

    Removing hacked posts.

--- MySQL/find_all_wordpress_posts_where_comments_are_allowed.sql
+++ MySQL/find_all_wordpress_posts_where_comments_are_allowed.sql
@@ -6,3 +6,45 @@ and post_author = 2
 and post_type = "post"
 and post_status = "publish"
 LIMIT 0 , 4000
+
+Removing Hacked Posts from rh_posts table.
+describe rh_posts;
++-----------------------+---------------------+------+-----+---------------------+----------------+
+| Field                 | Type                | Null | Key | Default             | Extra          |
++-----------------------+---------------------+------+-----+---------------------+----------------+
+| ID                    | bigint(20) unsigned | NO   | PRI | NULL                | auto_increment |
+| post_author           | bigint(20) unsigned | NO   | MUL | 0                   |                |
+| post_date             | datetime            | NO   |     | 0000-00-00 00:00:00 |                |
+| post_date_gmt         | datetime            | NO   |     | 0000-00-00 00:00:00 |                |
+| post_content          | longtext            | NO   |     | NULL                |                |
+| post_title            | text                | NO   |     | NULL                |                |
+| post_excerpt          | text                | NO   |     | NULL                |                |
+| post_status           | varchar(20)         | NO   |     | publish             |                |
+| comment_status        | varchar(20)         | NO   |     | open                |                |
+| ping_status           | varchar(20)         | NO   |     | open                |                |
+| post_password         | varchar(255)        | NO   |     |                     |                |
+| post_name             | varchar(200)        | NO   | MUL |                     |                |
+| to_ping               | text                | NO   |     | NULL                |                |
+| pinged                | text                | NO   |     | NULL                |                |
+| post_modified         | datetime            | NO   |     | 0000-00-00 00:00:00 |                |
+| post_modified_gmt     | datetime            | NO   |     | 0000-00-00 00:00:00 |                |
+| post_content_filtered | longtext            | NO   |     | NULL                |                |
+| post_parent           | bigint(20) unsigned | NO   | MUL | 0                   |                |
+| guid                  | varchar(255)        | NO   |     |                     |                |
+| menu_order            | int(11)             | NO   |     | 0                   |                |
+| post_type             | varchar(20)         | NO   | MUL | post                |                |
+| post_mime_type        | varchar(100)        | NO   |     |                     |                |
+| comment_count         | bigint(20)          | NO   |     | 0                   |                |
++-----------------------+---------------------+------+-----+---------------------+----------------+
+23 rows in set (0.00 sec)
+
+select id, post_content, post_title from rh_posts where post_title
+like '%Caught deadly%';
+
+select post_content from rh_posts where id = 3922;
+  update rh_posts set post_content = 'Testing 20180226_3922' where id = 3922;
+select post_content from rh_posts where id = 3923; -- OK
+select post_content from rh_posts where id = 4029;
+  update rh_posts set post_content = 'Testing 20180226_4029' where id = 4029;
+select post_content from rh_posts where id = 4039;
+  update rh_posts set post_content = 'Testing 20180226_4039' where id = 4039;
