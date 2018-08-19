bac59fb8ab7df790fe77386d3c9386a5244f4d13
Robert Holland <Robert.Holland@bannerhealth.com>
2018-08-19 11:45:02 -0700

    Updated create_users.sql  query to grant superuser status to a specific user.

--- MySQL/create_users.sql
+++ MySQL/create_users.sql
@@ -12,3 +12,7 @@ CREATE USER 'admin'@'localhost';
 GRANT RELOAD,PROCESS ON *.* TO 'admin'@'localhost';
 
 CREATE USER 'dummy'@'localhost';
+
+MySQL 8.0.X.X. Grand superuser privileges:
+UPDATE `user` SET `Select_priv` = 'Y', `Insert_priv` = 'Y', `Update_priv` = 'Y', `Delete_priv` = 'Y', `Create_priv` = 'Y', `Drop_priv` = 'Y', `Reload_priv` = 'Y', `Shutdown_priv` = 'Y', `Process_priv` = 'Y', `File_priv` = 'Y', `Grant_priv` = 'Y', `References_priv` = 'Y', `Index_priv` = 'Y', `Alter_priv` = 'Y', `Show_db_priv` = 'Y', `Super_priv` = 'Y', `Create_tmp_table_priv` = 'Y', `Lock_tables_priv` = 'Y', `Execute_priv` = 'Y', `Repl_slave_priv` = 'Y', `Repl_client_priv` = 'Y', `Create_view_priv` = 'Y', `Show_view_priv` = 'Y', `Create_routine_priv` = 'Y', `Alter_routine_priv` = 'Y', `Create_user_priv` = 'Y', `Event_priv` = 'Y', `Trigger_priv` = 'Y', `Create_tablespace_priv` = 'Y', `Create_role_priv` = 'Y', `Drop_role_priv` = 'Y' WHERE `user`.`Host` = '%' AND `user`.`User` = 'rob';
+
