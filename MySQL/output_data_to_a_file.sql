3d425847ed7654fbfbc3e85807f0d22f0c1e3266
Robert Holland <rob@robholland.com>
2018-09-03 08:27:10 -0700

    Consolidated mysql_query_from_the_commandline.sql and output_data_to_a_file.sql. Everything is now in output_data_to_a_file.sql.

--- MySQL/output_data_to_a_file.sql
+++ MySQL/output_data_to_a_file.sql
@@ -1,18 +1,23 @@
 
--- From the mysql command prompt you can create an output file formatted with comma delimiters and fields enclosed by quotes.
+-- From the mysql command prompt you can create an output file formatted with comma delimiters and fields enclosed by quotes. The mysql user must have privileges to the output file location.
+select * into
+outfile 'outputfilename.txt'
+fields terminated by ','
+optionally enclosed by '"'
+lines terminated by '\n'
+from yourtablename
+where columnname = 'whatever';
 
-select * into outfile 'outputfilename.txt' fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from yourtablename where columnname = 'whatever';
-
--- The file will be located in the database folder. In my case (Fedora 12) it was located in /var/lib/mysql/play/nfaweapons.txt.
+-- If no path is specified, the file will be located in the default database folder. In my case (Fedora 12) it was located in /var/lib/mysql/play/filename.txt.
 
 -- You can specify where the output file will go:
-SELECT id, 
+SELECT id,
    client,
    project,
    task,
-   description, 
+   description,
    time,
-   date  
+   date
   INTO OUTFILE '/path/to/file.csv' --mysql user must have privileges to the location.
   FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
@@ -21,10 +26,10 @@ SELECT id,
 
 -- From outside of the mysql command prompt (ie. the command line) you can also run a query and create an output file with the results of that query.
 
--- Create a query that you want to run and save it as an .sql file. This example uses macaddress.sql.
+-- Create a query that you want to run and save it as an .sql file.
 
 -- The content of the test.sql file looks like this:
-	select * from tablename;
+select * from tablename;
 
 -- To run test.sql type one of the following:
 
@@ -37,13 +42,16 @@ mysql databasename -u username -p --xml < test.sql > output.xml
 -- If you don't want to create the test.sql file and just want to run everything from one commandline statement and get XML output:
 mysql -u username -p --xml -e 'use databasename; select * from tablename' > output.xml
 
--- You can also put the db_name in your script to avoid typing it on the command line.
+-- You can also put the db_name in your script to avoid typing it on the command line (see below).
 mysql -u username -p --xml < test.sql > output.xml
 
 -- You can create a bash script named getdata and put the following in it.
 mysql -u username -p < test.sql > test.tab
--- Then chmod +x getdata. then run it from the commandline: sh getdata. 
+-- Then chmod +x getdata. then run it from the commandline: sh getdata.
 -- You will be prompted for your password and the data will get dumped to test.tab.
+
+-- Connect to a remote database server and directly to a database on that server.
+mysql -h hostname -u databaseuser -p -D databasename
 
 -- To Dump a database to a file and generate full insert statments:
 mysqldump -p -c -e databasename > DatabaseName.sql
@@ -54,6 +62,18 @@ mysqldump -p -c -e databasename tablename > TableName.sql
 -- To dump all databases on the database server:
 mysqldump -p -c -e --all-databases > AllDatabases.sql
 
+-- To dump all databases on the database server without the data (just a skeleton).
+mysqldump -p -c -e --no-data --all-databases > AllDatabases_Skeleton.sql
+
+-- To dump a database from a remote server:
+mysqldump -h hostname -u username -p -c -e databasename > NameOfDatabaseDumpFile.sql
+
+-- To dump a table from a database on a remote server:
+mysqldump -h hostname -u username -p -c -e databasename tablename > NameOfTableDumpFile.sql
+
+-- To dump all databases on a remote server:
+mysqldump -h hostname -u username -p -c -e --all-databases > NameOfEntireDatabaseDumpFile.sql
+
 -- The contents of test.sql without the database name.
 select * from tablename order by columnName;
 
@@ -61,4 +81,9 @@ select * from tablename order by columnName;
 use database;
 select * from tablename order by columnName;
 
-
+-- Check and Repair Database
+systemctl stop mariadb
+myisamchk -r -q database.frm
+myisamchk -r -q database.MYI
+myisamchk -r -q database.MYD
+systemctl start mariadb
