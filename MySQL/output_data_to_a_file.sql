4cfba919adc88e9db860f793aaac243fcc42f42d
Robert Holland <rob@robholland.com>
2022-04-13 22:26:05 -0700

    Minor change to output_data_to_a_file.sql.

--- MySQL/output_data_to_a_file.sql
+++ MySQL/output_data_to_a_file.sql
@@ -34,30 +34,30 @@ select * from tablename;
 -- To run test.sql type one of the following:
 
 -- Tab delimited output format (default):
-mysql databasename -u username -p < test.sql > output.tab
+mysql thedatabasename -u theusername -p < test.sql > output.tab
 
 -- XML output format:
-mysql databasename -u username -p --xml < test.sql > output.xml
+mysql thedatabasename -u theusername -p --xml < test.sql > output.xml
 
 -- If you don't want to create the test.sql file and just want to run everything from one commandline statement and get XML output:
-mysql -u username -p --xml -e 'use databasename; select * from tablename' > output.xml
+mysql -u theusername -p --xml -e 'use databasename; select * from tablename' > output.xml
 
 -- You can also put the db_name in your script to avoid typing it on the command line (see below).
-mysql -u username -p --xml < test.sql > output.xml
+mysql -u theusername -p --xml < test.sql > output.xml
 
 -- You can create a bash script named getdata and put the following in it.
-mysql -u username -p < test.sql > test.tab
+mysql -u theusername -p < test.sql > test.tab
 -- Then chmod +x getdata. then run it from the commandline: sh getdata.
 -- You will be prompted for your password and the data will get dumped to test.tab.
 
 -- Connect to a remote database server and directly to a database on that server.
-mysql -h hostname -u databaseuser -p -D databasename
+mysql -h thehostname -u thedatabaseuser -p -D thedatabasename
 
 -- To Dump a database to a file and generate full insert statments:
-mysqldump -p -c -e databasename > DatabaseName.sql
+mysqldump -p -c -e thedatabasename > DatabaseName.sql
 
 -- To dump a table from a database and generate full insert statements:
-mysqldump -p -c -e databasename tablename > TableName.sql
+mysqldump -p -c -e thedatabasename thetablename > TableName.sql
 
 -- To dump all databases on the database server:
 mysqldump -p -c -e --all-databases > AllDatabases.sql
@@ -66,20 +66,20 @@ mysqldump -p -c -e --all-databases > AllDatabases.sql
 mysqldump -p -c -e --no-data --all-databases > AllDatabases_Skeleton.sql
 
 -- To dump a database from a remote server:
-mysqldump -h hostname -u username -p -c -e databasename > NameOfDatabaseDumpFile.sql
+mysqldump -h thehostname -u theusername -p -c -e thedatabasename > NameOfDatabaseDumpFile.sql
 
 -- To dump a table from a database on a remote server:
-mysqldump -h hostname -u username -p -c -e databasename tablename > NameOfTableDumpFile.sql
+mysqldump -h thehostname -u theusername -p -c -e thedatabasename thetablename > NameOfTableDumpFile.sql
 
 -- To dump all databases on a remote server:
-mysqldump -h hostname -u username -p -c -e --all-databases > NameOfEntireDatabaseDumpFile.sql
+mysqldump -h thehostname -u theusername -p -c -e --all-databases > NameOfEntireDatabaseDumpFile.sql
 
 -- The contents of test.sql without the database name.
-select * from tablename order by columnName;
+select * from thetablename order by columnName;
 
 -- The contents of test.sql with the database name.
 use database;
-select * from tablename order by columnName;
+select * from thetablename order by columnName;
 
 -- Check and Repair Database
 systemctl stop mariadb
@@ -87,3 +87,23 @@ myisamchk -r -q database.frm
 myisamchk -r -q database.MYI
 myisamchk -r -q database.MYD
 systemctl start mariadb
+
+Sometimes you will get an error when trying to create a data file:
+ERROR 1290 (HY000): The MySQL server is running with the "--secure-file-priv" option so it cannot execute this statement
+  
+  Run the following statement to show where you are allowed to save data:
+  SHOW VARIABLES LIKE "secure_file_priv";
+  
+  This is the path that showed when I typed the query:
+  rob@localhost [stibbard]> SHOW VARIABLES LIKE "secure_file_priv";
+  +------------------+-----------------------+
+  | Variable_name    | Value                 |
+  +------------------+-----------------------+
+  | secure_file_priv | /var/lib/mysql-files/ |
+  +------------------+-----------------------+
+  1 row in set (0.06 sec)
+  
+  I adjusted my query to include the file path that was shown:
+  select data into outfile '/var/lib/mysql-files/website_files.tar.gz.asc' fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from website_files where id = 1;
+  
+  The path was owned by the mysql user and group.
