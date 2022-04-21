fee090fe7707464b5cdc8e644474914c07980c00
Robert Holland <rob@robholland.com>
2022-04-21 13:18:20 -0700

    MSSQL display create view statement for a view.

---
+++ MSSQL/display_create_view.sql
@@ -0,0 +1,9 @@
+Display the Create View statement used to create a view:
+
+EXEC sp_helptext 'name_of_the_view';
+
+Example:
+	EXEC sp_helptext 'HumanResources.vEmployee';
+	EXEC sp_helptext 'vw_discern_email';
+
+ 
